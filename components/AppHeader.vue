590c09f6ffe83861c08afa816e40a7f83ba44abb
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2025-01-11 00:06:17 +0500

    changes

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -14,8 +14,8 @@ async function onLogout(): Promise<void> {
 <template>
   <header class="header">
     <NuxtLink href="/">Мебельный салон</NuxtLink>
-    <NuxtLink href="/">Главная</NuxtLink>
     <div v-if="store.user?.role">
+      <NuxtLink href="/">Главная</NuxtLink>
       <NuxtLink v-if="store.user?.role === UserRoleEnum.Buyer" href="cart">Корзина</NuxtLink>
       <NuxtLink v-if="store.user?.role === UserRoleEnum.Admin" href="orders">Заказы</NuxtLink>
       <span>
@@ -41,6 +41,12 @@ async function onLogout(): Promise<void> {
   font-size: 24px;
   border-bottom-left-radius: 16px;
   border-bottom-right-radius: 16px;
+
+  div {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+  }
 
   a {
     text-decoration: none;
